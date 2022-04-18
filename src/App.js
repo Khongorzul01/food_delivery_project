d9b8bf6029e5929b1deff60842a194dad465394b
mstars_lab2_06 <mstars_lab2_06@mstars-lab2-06s-iMac.local>
2022-04-18 14:35:32 +0800

    project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./components/Main";
+import Menu from "./components/Menu";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/menu" element={<Menu />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
